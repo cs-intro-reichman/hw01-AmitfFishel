76801de1c46110d49d05bfbaf6b1f8722a7cd91a
Amit Fishel <amit.fishel@post.runi.ac.il>
2023-12-20 17:58:30 +0200

    solved exericse

--- GenThree.java
+++ GenThree.java
@@ -1,9 +1,20 @@
-/*
- * Generates three random integers, each in a given range [a,b),
- * prints them, and then prints the minimal number that was generated.
- */
-public class GenThree {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
-}
+public class Gen3 {
+        public static void main(String[] args) {
+            
+            // b is bigger than a.       
+
+            int a = Integer.parseInt(args[0]);
+            int b = Integer.parseInt(args[1]);
+            int randomNum1 = (int)(Math.random() * (b-a)) + a;
+            int randomNum2 = (int)(Math.random() * (b-a)) + a;
+            int randomNum3 = (int)(Math.random() * (b-a)) + a;
+
+
+            System.out.println(randomNum1);
+            System.out.println(randomNum2);
+            System.out.println(randomNum3);
+            System.out.println("The minimal generated number was " + Math.min(randomNum1, Math.min(randomNum2, randomNum3))); 
+
+	
+        }
+    }
